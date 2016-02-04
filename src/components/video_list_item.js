48c363bca9fb2032fe6383a02fff12783d5626b3
avivalk <aviva.kerstein@huffingtonpost.com>
2016-02-04 13:32:29 -0500

    Render Video Detail and Video List

---
+++ src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'React';
+
+const VideoListItem = ({video}) => {
+  // ({video}) is equivalent to (props) and then const video = props.video;
+
+  const imageURL = video.snippet.thumbnails.default.url;
+  return(
+   <li className = 'list-group-item'>
+     <div className="video-list media">
+       <div className="media-left">
+        <img className="media-object" src={ imageURL } />
+       </div>
+       <div className="media-body">
+         <div className="media-heading">{ video.snippet.title }</div>
+       </div>
+     </div>
+    </li>
+  )
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-// Create a new component. This component should produce some HTML
 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyC3oDC8DnCPxW6Qxbbi-vlxG1702Z0CV_g'
+
+// Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // when key and property are same name this is cleaner syntax
+      // it's equivalent to this.setState( { videos:videos });
+      this.setState({ videos });
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={ this.state.videos } />
+      </div>
+    );
+  }
+}
 
 //Take this component's generated HTML and put it on the page(in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
